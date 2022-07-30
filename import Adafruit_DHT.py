403fa41a2e943cb430a46485d3db9f64cf2880d8
ninnofathan <108131425+ninnofathan@users.noreply.github.com>
2022-07-30 12:42:35 +0700

    Add files via upload

---
+++ import Adafruit_DHT.py
@@ -0,0 +1,9 @@
+import Adafruit_DHT
+ 
+sensor=Adafruit_DHT.DHT11
+pin=17
+ 
+while True:
+
+     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+     print('suhu={0:0.1f}*C kelembapan={1:0.1f}%'.format(temperature, humidity))
